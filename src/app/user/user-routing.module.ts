aa5d4b487ec5e064d2c9c2d6221935ba2db624b0
Shaikseema-design <seema@blazeautomation.com>
2025-03-24 19:02:55 +0530

    feat(add user-list and user-details in user module)
    add user.module in routing.module as lazy loading

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -4,8 +4,8 @@ import { UserListComponent } from './user-list/user-list.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
 
 const routes: Routes = [
-  { path: '', component: UserListComponent }, // Default route for user list
-  { path: ':id', component: UserDetailComponent }, // Route for user details
+  { path: '', component: UserListComponent }, //default route for user list
+  { path: ':id', component: UserDetailComponent }, //route for user list
   
 ];
 
