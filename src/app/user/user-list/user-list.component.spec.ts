336e3e5ef7ba927432366bdc0dc211a1c66011a5
Shaikseema-design <seema@blazeautomation.com>
2025-03-25 18:10:43 +0530

    feat(add unit testing)

--- src/app/user/user-list/user-list.component.spec.ts
+++ src/app/user/user-list/user-list.component.spec.ts
@@ -1,23 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserListComponent } from './user-list.component';
+import { provideRouter, Router } from '@angular/router';
 
 describe('UserListComponent', () => {
   let component: UserListComponent;
   let fixture: ComponentFixture<UserListComponent>;
+  let router:Router;
 
   beforeEach(async () => {
+    const routerspy=jasmine.createSpyObj('Router',['navigate'])
     await TestBed.configureTestingModule({
-      declarations: [ UserListComponent ]
+      declarations: [ UserListComponent ],
+      providers:[
+        {provide:Router,useValue:routerspy},
+        provideRouter([])
+      ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(UserListComponent);
     component = fixture.componentInstance;
+    router=TestBed.inject(Router);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should show the details',()=>{
+    const userId=1;
+    component.viewDetails(userId);
+    expect(router.navigate).toHaveBeenCalledWith(['/user',userId]);
+  })
 });
